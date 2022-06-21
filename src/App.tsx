6c1294ce360fb0c0074ad140129b08a42fe4e4dd
reistie1 <josh.reist@gmail.com>
2022-06-20 21:56:36 -0400

    began creating portfolio layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <a href="about_me">About Me</a>
+        <a href="languages-frameworks">Languages/Frameworks</a>
+        <a href="projects">Projects</a>
       </header>
+      <div className="picture-frame">
+        <div className="picture">
+        </div>
+        <section className="person-name-section">
+          <p className="person-name">Josh Reist</p>
+        </section>
+      </div>
+      <div>
+        <section>
+          <h2 id="about_me">About Me</h2>
+          <p>Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.</p>
+        </section>
+      </div>
+      <div>
+        <h2 id="languages-frameworks">Languages/Frameworks</h2>
+        <section className="language-wrapper">
+          <div className="language-item">
+            <h3>Javascript</h3>
+          </div>
+          <div className="language-item">
+            <h3>HTML5</h3>
+          </div>
+          <div className="language-item">
+            <h3>CSS3</h3>
+          </div>
+          <div className="language-item">
+            <h3>C#</h3>
+          </div>
+          <div className="language-item">
+            <h3>Azure</h3>
+          </div>
+          <div className="language-item">
+            <h3>Reactjs</h3>
+          </div>
+          <div className="language-item">
+            <h3>SQL</h3>
+          </div>
+          <div className="language-item">
+            <h3>UML</h3>
+          </div>
+          <div className="language-item">
+            <h3>MangoDB</h3>
+          </div>
+          <div className="language-item">
+            <h3></h3>
+          </div>
+        </section>
+      </div>
+      <div>
+        <section>
+          <h2 id="projects">Projects</h2>
+        </section>
+      </div>
     </div>
   );
 }
