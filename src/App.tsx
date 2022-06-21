a6a91afbf4bfe2b71f181701655b292592e81cef
reistie1 <josh.reist@gmail.com>
2022-06-20 22:33:30 -0400

    added projects to portfolio layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header>
-        <a href="about_me">About Me</a>
-        <a href="languages-frameworks">Languages/Frameworks</a>
-        <a href="projects">Projects</a>
+        <a href="#about_me">About Me</a>
+        <a href="#languages-frameworks">Languages/Frameworks</a>
+        <a href="#projects">Projects</a>
       </header>
       <div className="picture-frame">
         <div className="picture">
@@ -16,8 +16,8 @@ function App() {
           <p className="person-name">Josh Reist</p>
         </section>
       </div>
-      <div>
-        <section>
+      <div className="about-me-wrapper">
+        <section >
           <h2 id="about_me">About Me</h2>
           <p>Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.Loren ipsum something is written here.</p>
         </section>
@@ -47,19 +47,60 @@ function App() {
             <h3>SQL</h3>
           </div>
           <div className="language-item">
-            <h3>UML</h3>
-          </div>
-          <div className="language-item">
             <h3>MangoDB</h3>
           </div>
           <div className="language-item">
-            <h3></h3>
+            <h3>Nodejs</h3>
+          </div>
+          <div className="language-item">
+            <h3>Typescript</h3>
+          </div>
+          <div className="language-item">
+            <h3>Azure CI/CD</h3>
           </div>
         </section>
       </div>
       <div>
-        <section>
-          <h2 id="projects">Projects</h2>
+        <h2 style={{marginBottom: "1rem"}}>Projects</h2>
+        <section className="project-item">
+          <h4 id="projects">Algorithms</h4>
+          <div className="project-img-wrapper">
+            <div className="project-img">
+            </div>
+            <div className="project-information">
+              <p>Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.</p>
+            </div>
+          </div>
+        </section>
+        <section className="project-item">
+          <h4 id="projects">Retailer App</h4>
+          <div className="project-img-wrapper">
+            <div className="project-img">
+            </div>
+            <div className="project-information">
+              <p>Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.</p>
+            </div>
+          </div>
+        </section>
+        <section className="project-item">
+          <h4 id="projects">Scout Stat V2</h4>
+          <div className="project-img-wrapper">
+            <div className="project-img">
+            </div>
+            <div className="project-information">
+              <p>Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.</p>
+            </div>
+          </div>
+        </section>
+        <section className="project-item">
+          <h4 id="projects">Production App</h4>
+          <div className="project-img-wrapper">
+            <div className="project-img">
+            </div>
+            <div className="project-information">
+              <p>Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.Loren ipsum something long description.</p>
+            </div>
+          </div>
         </section>
       </div>
     </div>
